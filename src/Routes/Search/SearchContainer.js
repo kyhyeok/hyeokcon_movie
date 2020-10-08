cf7fb44e716b94a82e53148a55779aa989e1739b
kyhyeok <kyhyeok@gmail.com>
2020-10-08 15:43:54 +0900

    Search Container Finished

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { movieApi, tvApi } from "../../api";
 import SearchPresenter from "./SearchPresenter";
 
 export default class extends React.Component {
@@ -9,6 +10,35 @@ export default class extends React.Component {
     error: null,
     loading: false
   };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const {
+        data: { results: movieResult }
+      } = await movieApi.search(searchTerm);
+      const {
+        data: { results: tvResult }
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResult,
+        tvResult
+      });
+      this.setState({ loading: true });
+    } catch {
+      this.setState({ error: "Can't  find results" });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { movieResult, tvResult, searchTerm, error, loading } = this.state;
     return (
@@ -18,6 +48,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
