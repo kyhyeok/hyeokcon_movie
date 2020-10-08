a37d7b4e1dce198e580c1779c33347fdd5d02c9a
kyhyeok <kyhyeok@gmail.com>
2020-10-08 19:17:18 +0900

    SearchPresenter Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
 
 const SearchPresenter = ({
   movieResult,
@@ -8,8 +24,39 @@ const SearchPresenter = ({
   error,
   loading,
   searchTerm,
-  handleSubmit
-}) => null;
+  handleSubmit,
+  updateTerm
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="Search Movies or TV Shows..."
+        value={searchTerm}
+        onChange={updateTerm}
+      />
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResult?.length > 0 && (
+          <Section title="Movie Results">
+            {movieResult.map((movie) => (
+              <span key={movie.id}>{movie.title}</span>
+            ))}
+          </Section>
+        )}
+        {tvResult?.length > 0 && (
+          <Section title="Tv Show Results">
+            {tvResult.map((show) => (
+              <span key={show.id}>{show.name}</span>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
+  </Container>
+);
 
 SearchPresenter.propTypes = {
   movieResult: PropTypes.array,
@@ -17,7 +64,8 @@ SearchPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
   searchTerm: PropTypes.string,
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired
 };
 
 export default SearchPresenter;
