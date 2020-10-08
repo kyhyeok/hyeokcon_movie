7174fd01c9da4af57e63531a9ea326b1280ca67b
kyhyeok <kyhyeok@gmail.com>
2020-10-08 13:01:37 +0900

    API Verbs part one

--- src/api.js
+++ src/api.js
@@ -8,4 +8,16 @@ const api = axios.create({
   }
 });
 
+const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular")
+};
+
+const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today")
+};
+
 export default api;
