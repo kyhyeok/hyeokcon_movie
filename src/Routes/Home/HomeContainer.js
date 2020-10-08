f8c062453ec94f97a6e1ec797fe6bfaeb15d4c6f
kyhyeok <kyhyeok@gmail.com>
2020-10-08 15:11:24 +0900

    TV Container Finished

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -29,7 +29,7 @@ export default class extends React.Component {
       });
     } catch {
       this.setState({
-        error: "Can't find movies information."
+        error: "Can't find Movie information."
       });
     } finally {
       this.setState({
@@ -40,7 +40,6 @@ export default class extends React.Component {
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
-    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
